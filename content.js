362056947e11045cc85c02074509db9f842684cc
utkucanozturk <utkucanozturk@gmail.com>
2022-02-02 11:18:15 +0100

    added switch to the blocking

--- content.js
+++ content.js
@@ -1,16 +1,21 @@
-const popupTagNames = ['DIV', 'SECTION', 'FOOTER', 'ASIDE', 'FORM']
+const hostnameRegex = /\/\/([\.\-\_\w]+)/i;
+
+function hide() {
+  const blockSign = document.getElementsByClassName("is-blocked");
+  blockSign.style.display = "block";
+}
+
+const popupTagNames = ["DIV", "SECTION", "FOOTER", "ASIDE", "FORM"];
 
 function isFixed(node) {
-  return window.getComputedStyle(node).position === 'fixed'
+  return window.getComputedStyle(node).position === "fixed";
 }
 
 function isCookieNotice(node) {
-  const attrs = [
-    node.id,
-    node.className,
-    node.getAttribute('aria-label')
-  ]
-  return attrs.some(attr => /(cookie|gdpr|consent|privacy|opt-in)/i.test(attr))
+  const attrs = [node.id, node.className, node.getAttribute("aria-label")];
+  return attrs.some((attr) =>
+    /(cookie|gdpr|consent|privacy|opt-in)/i.test(attr)
+  );
 }
 
 function inspectAndStrip(nodeList) {
@@ -18,64 +23,54 @@ function inspectAndStrip(nodeList) {
     if (isCookieNotice(node)) {
       // check if position:fixed
       if (isFixed(node)) {
-        node.remove()
-        return true
+        node.remove();
+        return true;
       }
 
       // else check if div is on top screen border
-      const rect = node.getBoundingClientRect()
+      const rect = node.getBoundingClientRect();
       if (rect.top == 0) {
-        node.remove()
-        return true
+        node.remove();
+        return true;
       }
 
       // else search for fixed node in direct children
       for (const child of node.children) {
         if (isFixed(child)) {
-          child.remove()
-          return true
+          child.remove();
+          return true;
         }
       }
     }
-    return blocked
-  }, false)
+    return blocked;
+  }, false);
 }
 
-// run initially (after dom content loaded)
+var observer = new MutationObserver((mutationRecords) => {
+  let addedNodes = [];
 
-export function main(){
-  const hostname = window.location.hostname
-  browser.storage.sync.get(hostname).then(res => {
-    if (res[hostname] == 'i') {
-      browser.runtime.sendMessage('ignored')
-    } else {
-      const blocked = popupTagNames.reduce((res, tag) => {
-        const nodes = document.querySelectorAll(tag)
-        if (inspectAndStrip(nodes)) res = true
-        return res
-      }, false)
-      if (blocked) browser.runtime.sendMessage('blocked')
-      createObserver()
-    }
-  })
+  for (const mut of mutationRecords) {
+    let ary = Array.prototype.slice.call(mut.addedNodes);
+    ary = ary.filter((node) => popupTagNames.includes(node.tagName));
+
+    addedNodes = addedNodes.concat(ary);
+  }
+
+  const blocked = inspectAndStrip(addedNodes);
+  if (blocked) hide();
+});
+
+const blocked = popupTagNames.reduce((res, tag) => {
+  const nodes = document.querySelectorAll(tag);
+  if (inspectAndStrip(nodes)) res = true;
+  return res;
+}, false);
+
+if (blocked) {
+  const blockSign = document.getElementsByClassName("is-blocked");
+  blockSign.style.display = "block";
 }
 
+const config = { attributes: false, childList: true, subtree: true };
 
-function createObserver() {
-  // callback function to execute when mutations are observed
-  const observer = new MutationObserver(mutationRecords => {
-    let addedNodes = []
-
-    for (const mut of mutationRecords) {
-      let ary = Array.prototype.slice.call(mut.addedNodes)
-      ary = ary.filter(node => popupTagNames.includes(node.tagName))
-
-      addedNodes = addedNodes.concat(ary)
-    }
-
-    const blocked = inspectAndStrip(addedNodes)
-    if (blocked) browser.runtime.sendMessage('blocked')
-  })
-  const config = { attributes: false, childList: true, subtree: true }
-  observer.observe(document.body, config)
-}
+observer.observe(document.body, config);
--- cookies.js
+++ cookies.js
@@ -2,32 +2,57 @@ function showCookiesForTab(tabs) {
     let tab = tabs.pop();
     var gettingAllCookies = browser.cookies.getAll({ url: tab.url });
     gettingAllCookies.then((cookies) => {
-        var activeTabUrl = document.getElementById('header-title');
-        var text = document.createTextNode("Cookies at: " + tab.title);
-        activeTabUrl.appendChild(text);
-        var cookieList = document.getElementById('cookie-list');
-        if (cookies.length > 0) {
-            for (let cookie of cookies) {
-                let li = document.createElement("li");
-                let content = document.createTextNode(cookie.name + ": " + cookie.value);
-                li.appendChild(content);
-                cookieList.appendChild(li);
-            }
-        } else {
-            let p = document.createElement("p");
-            let content = document.createTextNode("No cookies in this tab.");
-            let parent = cookieList.parentNode;
-            p.appendChild(content);
-            parent.appendChild(p);
+      var activeTabUrl = document.getElementById("header-title");
+      var text = document.createTextNode("Cookies at: " + tab.title);
+      activeTabUrl.appendChild(text);
+      var cookieList = document.getElementById("cookie-list");
+      if (cookies.length > 0) {
+        for (let cookie of cookies) {
+          let li = document.createElement("li");
+          let content = document.createTextNode(
+            cookie.name + ": " + cookie.value
+          );
+          li.appendChild(content);
+          cookieList.appendChild(li);
         }
+      } else {
+        let p = document.createElement("p");
+        let content = document.createTextNode("No cookies in this tab.");
+        let parent = cookieList.parentNode;
+        p.appendChild(content);
+        parent.appendChild(p);
+      }
     });
-}
-
-function getActiveTab() {
+  }
+  
+  function getActiveTab() {
     return browser.tabs.query({ currentWindow: true, active: true });
-}
-
-getActiveTab().then(showCookiesForTab);
-
-
-
+  }
+  
+  getActiveTab().then(showCookiesForTab);
+  
+  const checkbox = document.querySelector("input");
+  
+  const hostnameRegex = /\/\/([\.\-\_\w]+)/i;
+  
+  checkbox.onchange = function (e) {
+    getActiveTab().then((tabs) => {
+      const url = tabs[0].url;
+      if (!e.target.checked) {
+        browser.cookies.set({
+          url,
+          name: "no-cookies-please",
+          value: "off",
+        });
+  
+        // reload so can see if current pop-up exists
+        browser.tabs.reload(tabs[0].id);
+      } else {
+        browser.cookies.remove({
+          url,
+          name: "no-cookies-please",
+        });
+      }
+    });
+  };
+  
